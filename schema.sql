687f30a2fefde865e5b58a86e6ca1f3d23ce4812
mutazabutaha <mutazabutaha13@outlook.com>
2023-04-15 19:25:44 +0000

    update-and-delete

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS visitlist ;
  CREATE TABLE IF NOT EXISTS visitlist (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
-   city VARCHAR(255),
+    city VARCHAR(255),
     image VARCHAR(255),
     overview VARCHAR(2000),
     feedback VARCHAR(255)
